7b4cb97d0adac031a253e87d7a74fc1bc080deac
SiamKing <thesiameseelectric@gmail.com>
2017-01-07 20:55:22 -0800

    smog check

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,5 +1,5 @@
 describe('Favorite Food Filter', function () {
-	var $controller;
+	var $controller, FavoriteFood;
 
 	beforeEach(module('app'));
 
@@ -7,5 +7,28 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
-	
+	var mockedPeople = [{
+		name: "Jimmy",
+		favoriteFood: "Vegan Tacos"
+	},
+		{
+			name: "Tina",
+		favoriteFood: "Veggie Thai Curry"
+	},
+	{
+		name: "Veronica",
+		favoriteFood: "Seitan"
+	}];
+
+	beforeEach(inject(function ($filter) {
+		FavoriteFood = $filter('favoriteFood')
+	}));
+
+	it('should return the name of person with passed in food ', function () {
+		var results = $filter('favoriteFood')(mockedPeople, 'Vegan Tacos');
+
+		expect(results.length).toEqual(1);
+		expect(results[0].name).toEqual("Jimmy");
+	});
+
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -7,4 +7,12 @@ describe('removeAllVowels Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+	beforeEach(inject(function($filter) {
+		removeAllVowels = $filter('removeAllVowels');
+	}));
+
+	it('should remove all vowels from string', function() {
+		expect(removeAllVowels('Chicago')).toEqual('Chcg');
+	});
+
 });
